376d119f85ac3245f67ca701a86ccc2bae0f1386
Thomas Mirbey <101272724+ThomasM2568@users.noreply.github.com>
2023-09-25 15:59:59 +0200

    Create example.py

---
+++ Script/DataBase/example.py
@@ -0,0 +1,31 @@
+#-------------------------------------------
+#              Module Import
+#-------------------------------------------
+
+import argparse
+
+#-------------------------------------------
+#              Argument parsing
+#-------------------------------------------
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--arg', help='give an input for the script')
+
+args = parser.parse_args()
+
+#-------------------------------------------
+#                   Main
+#-------------------------------------------
+
+def main():
+  '''
+  @args:
+  None
+
+  @output:
+  Hello World
+  '''
+  print("Hello World")
+
+if __name__ == '__main__':
+  main()
