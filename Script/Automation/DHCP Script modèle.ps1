ac0c3197bb1b25c0413e37b708c1165ed4d132ed
ABerot-Armand <148052820+ABerot-Armand@users.noreply.github.com>
2023-10-19 10:04:27 +0200

    Script DHCP v1.ps1

--- Script/Automation/DHCP Script modèle.ps1
+++ Script/Automation/DHCP Script modèle.ps1
@@ -1,1 +1,6 @@
-﻿Add-DhcpServerV4Scope -Name "QUALITE" -StartRange "192.168.180.10" -EndRange "192.168.180.200" -SubnetMask "255.255.255.0" -State Active
+Add-DhcpServerv4Scope -Name "Administration" -StartRange "10.0.10.10" -EndRange "10.0.10.200" -SubnetMask "255.255.255.0" -State Active
+Add-DhcpServerv4Scope -Name "DMZ" -StartRange "10.0.20.10" -EndRange "10.0.20.200" -SubnetMask "255.255.255.0" -State Active
+Add-DhcpServerv4Scope -Name "Gestion" -StartRange "10.0.30.10" -EndRange "10.0.30.200" -SubnetMask "255.255.255.0" -State Active
+Add-DhcpServerv4Scope -Name "Voix" -StartRange "10.0.40.10" -EndRange "10.0.40.200" -SubnetMask "255.255.255.0" -State Active﻿
+Add-DhcpServerv4Scope -Name "Donnees" -StartRange "10.0.50.10" -EndRange "10.0.50.200" -SubnetMask "255.255.255.0" -State Active
+Add-DhcpServerv4Scope -Name "Medical" -StartRange "10.0.60.10" -EndRange "10.0.60.200" -SubnetMask "255.255.255.0" -State Active
