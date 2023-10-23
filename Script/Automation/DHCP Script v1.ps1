e9ff9e35a3bf4c0f0114f39bd0918918022b3770
ABerot-Armand <148052820+ABerot-Armand@users.noreply.github.com>
2023-10-23 10:46:03 +0200

    Update DHCP Script v1.ps1

--- Script/Automation/DHCP Script v1.ps1
+++ Script/Automation/DHCP Script v1.ps1
@@ -1,6 +1,17 @@
+# Ajoute une étendue DHCP nommée "Administration" pour des adresses de gestion
 Add-DhcpServerv4Scope -Name "Administration" -StartRange "10.0.10.10" -EndRange "10.0.10.200" -SubnetMask "255.255.255.0" -State Active
+
+# Ajoute une étendue DHCP nommée "DMZ" pour des adresses de la zone démilitarisée
 Add-DhcpServerv4Scope -Name "DMZ" -StartRange "10.0.20.10" -EndRange "10.0.20.200" -SubnetMask "255.255.255.0" -State Active
+
+# Ajoute une étendue DHCP nommée "Gestion" pour des adresses de gestion
 Add-DhcpServerv4Scope -Name "Gestion" -StartRange "10.0.30.10" -EndRange "10.0.30.200" -SubnetMask "255.255.255.0" -State Active
-Add-DhcpServerv4Scope -Name "Voix" -StartRange "10.0.40.10" -EndRange "10.0.40.200" -SubnetMask "255.255.255.0" -State Active﻿
-Add-DhcpServerv4Scope -Name "Donnees" -StartRange "10.0.50.10" -EndRange "10.0.50.200" -SubnetMask "255.255.255.0" -State Active
+
+# Ajoute une étendue DHCP nommée "Voix" pour des adresses vocales
+Add-DhcpServerv4Scope -Name "Voix" -StartRange "10.0.40.10" -EndRange "10.0.40.200" -SubnetMask "255.255.255.0" -State Active
+
+# Ajoute une étendue DHCP nommée "Données" pour des adresses de données
+Add-DhcpServerv4Scope -Name "Données" -StartRange "10.0.50.10" -EndRange "10.0.50.200" -SubnetMask "255.255.255.0" -State Active
+
+# Ajoute une étendue DHCP nommée "Medical" pour des adresses médicales
 Add-DhcpServerv4Scope -Name "Medical" -StartRange "10.0.60.10" -EndRange "10.0.60.200" -SubnetMask "255.255.255.0" -State Active
