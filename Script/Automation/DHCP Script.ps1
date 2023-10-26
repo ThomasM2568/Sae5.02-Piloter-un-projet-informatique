1647bed0b5297f7aef5cc9c9001fae23c2c83696
ABerot-Armand <148052820+ABerot-Armand@users.noreply.github.com>
2023-10-26 10:41:19 +0200

    Update and rename DHCP Script v1.ps1 to DHCP Script.ps1

--- Script/Automation/DHCP Script.ps1
+++ Script/Automation/DHCP Script.ps1
@@ -1,13 +1,5 @@
 Import-Module DhcpServer
 
-# Assignation de la route par défaut à chaque étendue
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.10.0 -Value "10.0.10.254"
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.20.0 -Value "10.0.20.254"
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.30.0 -Value "10.0.30.254"
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.40.0 -Value "10.0.40.254"
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.50.0 -Value "10.0.50.254"
-Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.60.0 -Value "10.0.60.254"
-
 # Ajoute une étendue DHCP nommée "Administration" pour des adresses de gestion
 Add-DhcpServerv4Scope -Name "Administration" -StartRange "10.0.10.10" -EndRange "10.0.10.200" -SubnetMask "255.255.255.0" -State Active
 
@@ -25,3 +17,14 @@ Add-DhcpServerv4Scope -Name "Données" -StartRange "10.0.50.10" -EndRange "10.0.
 
 # Ajoute une étendue DHCP nommée "Medical" pour des adresses médicales
 Add-DhcpServerv4Scope -Name "Medical" -StartRange "10.0.60.10" -EndRange "10.0.60.200" -SubnetMask "255.255.255.0" -State Active
+
+# Assignation de la route par défaut à chaque étendue
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.10.0 -Value "10.0.10.254"
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.20.0 -Value "10.0.20.254"
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.30.0 -Value "10.0.30.254"
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.40.0 -Value "10.0.40.254"
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.50.0 -Value "10.0.50.254"
+Set-DhcpServerv4OptionValue -OptionID 003 -ScopeId 10.0.60.0 -Value "10.0.60.254"
+
+# Ajoute la relation de basculement en mode de basculement de charge (50%)
+Add-DhcpServerv4Failover -Name "LoadBalancing" -ScopeID 10.0.10.0, 10.0.20.0, 10.0.30.0, 10.0.40.0, 10.0.50.0, 10.0.60.0 -LoadBalancePercent 50 -PartnerServer "ServBKupAkat.akat.fr" -SharedSecret "SAE502akat"
